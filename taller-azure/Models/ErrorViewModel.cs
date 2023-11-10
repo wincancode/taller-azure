d7a587ff57bd12f9549003ee1f358b434602dba1
Luis Barreto <luis.barreto.marin@gmail.com>
2023-11-10 15:18:47 -0400

    Add project files.

---
+++ taller-azure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace taller_azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
